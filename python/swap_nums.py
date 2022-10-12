6283d88550bf51a82bbbd40e042c72017c03736a
“Ravindu <ravindu.first@gmail.com>
2022-10-12 23:36:01 +0530

    added a python file

---
+++ python/swap_nums.py
@@ -0,0 +1,16 @@
+print("Enter the First Number: ", end="")
+a = int(input())
+print("Enter the Second Number: ", end="")
+b = int(input())
+
+print("\nBefore Swap")
+print("a =", a)
+print("b =", b)
+
+x = a
+a = b
+b = x
+
+print("\nAfter Swap")
+print("a =", a)
+print("b =", b)
